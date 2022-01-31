28c4dd3ccb5712dbe3c371673a2aaacb81b936a5
Akin <akinkavak1@gmail.com>
2022-01-31 15:34:37 +0300

    Adding event handlers

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+  
+    const onFocusHandler = () => console.log("Good!") 
+    const onBlurHandler = () => console.log("Hey! Eyes on me!")
+
+    return (
+        <button onFocus={onFocusHandler} onBlur={onBlurHandler}>Eyes on me</button>
+    )
+};
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+import React from "react"
+
+function Keypad() {
+
+    return (
+        <input type="password" onChange={() => console.log("Entering password...")}/>
+    )
+};
+
+export default Keypad;
